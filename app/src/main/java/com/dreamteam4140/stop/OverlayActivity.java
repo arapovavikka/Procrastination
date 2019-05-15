7f92d46506787c071f5f1aa656020ba665112b7a
kristinatr <kristinatroilova@gmail.com>
2019-05-15 23:48:54 +0300

    Disable back button on overlay

--- app/src/main/java/com/dreamteam4140/stop/OverlayActivity.java
+++ app/src/main/java/com/dreamteam4140/stop/OverlayActivity.java
@@ -2,6 +2,7 @@ package com.dreamteam4140.stop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,14 +24,18 @@ public class OverlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overlay);
         timer = new Timer();
-
-        timerText = (TextView) findViewById(R.id.timerText);
+        timerText = (TextView)findViewById(R.id.timerText);
         timer.start(timerText, 30);
 
         unlockPassword = AppPreferences.GetInstance(getApplicationContext()).getString(AppPreferences.Key.SETTINGS_PASSWORD_STR);
         //Toast.makeText(getApplicationContext(), "done! " + unlockPassword, Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Do nothing or catch the keys you want to block
+        return false;
+    }
     public void closeOverlay(View view) {
         finish();
     }
