c71e165bff62f3fccabc8fba5803be2733c38d84
Grigorii Kislyakov <grigorij.kislyakov@yandex.ru>
2019-05-17 02:51:45 +0300

    Unlock_password_Activity: 1)Has been added generate random number keyboard on the screen in UnlockPasswordActivity. 2)Some fixing and renaming in  activity_unlock_password.xml (naming + increase size of "X" on the screen)

--- app/src/main/java/com/dreamteam4140/stop/UnlockPasswordActivity.java
+++ app/src/main/java/com/dreamteam4140/stop/UnlockPasswordActivity.java
@@ -1,5 +1,6 @@
 package com.dreamteam4140.stop;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -7,25 +8,41 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class UnlockPasswordActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class UnlockPasswordActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText _passwordEditText;
     private Button _clearButton;
-
     private Button _upperLeftButton;
+
+    private HashMap<TextView,Integer> viewToInteger;
+
+    private Button button2;
+    private Button button3;
+
+    private TextView textView4;
+    private TextView textView5;
+    private TextView textView6;
+    private TextView textView7;
+    private TextView textView8;
+    private TextView textView9;
+    private TextView textView10;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unlock_password);
-
-        _clearButton = findViewById(R.id.buttonClear);
-
-        _upperLeftButton = findViewById(R.id.upperLeftButton);
-
-        _passwordEditText = findViewById(R.id.passwordEditText);
+        initialize();
+        setOnClickListeners();
         _passwordEditText.addTextChangedListener(new TextWatcher() {
 
             @Override
@@ -49,19 +66,84 @@ public class UnlockPasswordActivity extends AppCompatActivity {
             }
         });
 
-        _upperLeftButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                _passwordEditText.setText(_passwordEditText.getText().toString() + _upperLeftButton.getText());
-            }
-        });
 
-        _clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                _passwordEditText.setText("");
-                _clearButton.setVisibility(View.GONE);
-            }
-        });
+    }
+    @SuppressLint("CutPasteId")
+    private void initialize(){
+        ArrayList<Integer> randomArrayList = getRandomArrayList(0,9);
+        _clearButton = findViewById(R.id.buttonClear);
+
+        button2 = findViewById(R.id.button2);
+        button2.setText(String.valueOf(randomArrayList.get(0)));
+
+        button3 = findViewById(R.id.button3);
+        button3.setText(String.valueOf(randomArrayList.get(1)));
+
+        textView4 = findViewById(R.id.textView4);
+        textView4.setText(String.valueOf(randomArrayList.get(2)));
+
+        textView5 = findViewById(R.id.textView5);
+        textView5.setText(String.valueOf(randomArrayList.get(3)));
+
+        textView6 = findViewById(R.id.textView6);
+        textView6.setText(String.valueOf(randomArrayList.get(4)));
+
+        textView7 = findViewById(R.id.textView7);
+        textView7.setText(String.valueOf(randomArrayList.get(5)));
+
+        textView8 = findViewById(R.id.textView8);
+        textView8.setText(String.valueOf(randomArrayList.get(6)));
+
+        textView9 = findViewById(R.id.textView9);
+        textView9.setText(String.valueOf(randomArrayList.get(7)));
+
+        textView10 = findViewById(R.id.textView10);
+        textView10.setText(String.valueOf(randomArrayList.get(8)));
+
+        _upperLeftButton = findViewById(R.id.upperLeftButton);
+        _upperLeftButton.setText(String.valueOf(randomArrayList.get(9)));
+
+        _passwordEditText = findViewById(R.id.passwordEditText);
+
+    }
+
+    private ArrayList<Integer> getRandomArrayList (int start,int end ){
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+
+            arrayList.add(i);
+        }
+
+        Collections.shuffle(arrayList);
+       return arrayList;
+    }
+    private void setOnClickListeners() {
+        _upperLeftButton.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+        button3.setOnClickListener(this);
+        textView4.setOnClickListener(this);
+        textView5.setOnClickListener(this);
+        textView6.setOnClickListener(this);
+        textView7.setOnClickListener(this);
+        textView8.setOnClickListener(this);
+        textView9.setOnClickListener(this);
+        textView10.setOnClickListener(this);
+        _clearButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.buttonClear){
+                 _passwordEditText.setText("");
+                    _clearButton.setVisibility(View.GONE);
+                }
+
+
+        TextView clickedTextView  = findViewById(v.getId());
+        _passwordEditText.setText(_passwordEditText.getText().toString()+
+                clickedTextView.getText().toString());
+
     }
 }
