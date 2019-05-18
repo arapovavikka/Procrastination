3b2f6e50d4881275da69802752a7a61234d7151d
Lavenderist <viktoria.arapova@notissimus.com>
2019-05-18 19:35:32 +0300

    RangeSeekBar fixed

---
+++ app/src/main/java/com/dreamteam4140/stop/TortureActivity.java
@@ -0,0 +1,13 @@
+package com.dreamteam4140.stop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TortureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_torture);
+    }
+}
